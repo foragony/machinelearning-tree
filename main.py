39d0372c72de5e6a71464fa413893769d688d970
foragony <1045952912@qq.com>
2021-03-29 19:42:10 +0800

    决策树作业，只写了信息增益的函数

---
+++ main.py
@@ -0,0 +1,59 @@
+
+
+import numpy as np
+
+data = [["青绿", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.697],
+        ["乌黑", "蜷缩", "沉闷", "清晰", "凹陷", "硬滑", 0.774],
+        ["乌黑", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.634],
+        ["青绿", "蜷缩", "沉闷", "清晰", "凹陷", "硬滑", 0.608],
+        ["浅白", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.556],
+        ["青绿", "稍蜷", "浊响", "清晰", "稍凹", "软粘", 0.403],
+        ["乌黑", "稍蜷", "浊响", "稍糊", "稍凹", "软粘", 0.481],
+        ["乌黑", "稍蜷", "浊响", "清晰", "稍凹", "硬滑", 0.437],
+        ["乌黑", "稍蜷", "沉闷", "稍糊", "稍凹", "硬滑", 0.666],
+        ["青绿", "硬挺", "清脆", "清晰", "平坦", "软粘", 0.243],
+        ["浅白", "硬挺", "清脆", "模糊", "平坦", "硬滑", 0.245],
+        ["浅白", "蜷缩", "浊响", "模糊", "平坦", "软粘", 0.343],
+        ["青绿", "稍蜷", "浊响", "稍糊", "凹陷", "硬滑", 0.639],
+        ["浅白", "稍蜷", "沉闷", "稍糊", "凹陷", "硬滑", 0.657],
+        ["乌黑", "稍蜷", "浊响", "清晰", "稍凹", "软粘", 0.360],
+        ["浅白", "蜷缩", "浊响", "模糊", "平坦", "硬滑", 0.593]
+        ]
+data = np.array(data)
+# print(data[:, 2])
+
+# bbb = [[0],
+#        [0],
+#        [0]]
+bbb = []
+for i in range(3):
+    aaa = []
+    bbb.append(aaa)
+
+print(bbb)
+for i in range(3):
+    # aaa = []
+    bbb[i].append(525)
+print(bbb)
+# print(bbb[1][1:])
+# 利用函数递归创建决策树
+# dataSet：数据集
+# labels：标签列表，包含了数据集中所有特征的标签
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]    # 取出dataSet最后一列的数据
+
+    if classList.count(classList[0]) == len(classList): # classList中classList[0]出现的次数=classList长度，表示类别完全相同，停止继续划分
+        return classList[0]
+    if len(dataSet[0]) == 1:                            # 遍历完所有特征时返回出现次数最多的类别
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)        # 计算划分的最优特征（下标）
+    bestFeatLabel = labels[bestFeat]                    # 数据划分的最优特征的标签（即是什么特征）
+    myTree = {bestFeatLabel:{}}                         # 创建一个树（字典），bestFeatLabel为根结点
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)# 利用递归构造决策树
+    return myTree
---
+++ tree.py
@@ -0,0 +1,134 @@
+"""
+file: tree.py
+author: 1851995刘佳航
+基于信息熵进行划分选择的决策树算法,无剪枝
+"""
+
+import numpy as np
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+决策树数据结构构造
+可以利用字典构造决策树
+思路如下
+先找到最优的划分属性，然后这样构造字典
+treenode = {最佳属性：{里面是属性的各个取值}}
+本来想利用类来构造决策树，但是怎么构造没有思路，就去网上找了字典的构造方法
+"""
+
+def MaxIndex(_data):
+    _max = _data[0]
+    _index = 0
+    for i in range(len(_data)):
+        if _max < _data[i]:
+            _max = _data[i]
+            _index = i
+    return _max, _index
+
+
+def EntD(_label):
+    """
+    计算信息熵
+    :param _label: list
+    :return: double
+    """
+    length = len(_label)
+    _result = 0.0
+    for i in range(2):
+        p = _label.count(i) / length
+        _result -= p * np.log2(p)
+    return _result
+
+
+def Gain(_data, _label, _data_label):
+    _length = len(_label)
+    _result = 0.0
+    _each_data = []
+    for i in range(len(_data_label)):
+        _each_data.append([])
+    for i in range(_length):
+        for j in range(len(_data_label)):
+            if _data_label[j] == _data[i]:
+                _each_data[j].append(_label[i])
+                break
+    _result = EntD(_label)
+    for i in range(_data_label):
+        _result -= (len(_each_data[i])/_length)*EntD(_each_data[i])
+    return _result
+
+
+def ContinuationGain(_data, _label, _value):
+    """
+    :param _value: double
+    :param _data:  ndarray
+    :param _label:  list
+    :return:
+    """
+    _length = len(_label)
+    _result = 0.0
+    _data0 = []
+    _label0 = []
+    _data1 = []
+    _label1 = []
+    count = 0
+    for i in range(_length):
+        if _data[i] < _value:
+            count += 1
+            _data0.append(_data[i])
+            _label0.append((_label[i]))
+        else:
+            _data1.append(_data[i])
+            _label1.append((_label[i]))
+    _result = EntD(_label) - (count / _length) * EntD(_label0) - (1 - count / _length) * EntD(_label1)
+    return _result
+
+
+data = [["青绿", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.697],
+        ["乌黑", "蜷缩", "沉闷", "清晰", "凹陷", "硬滑", 0.774],
+        ["乌黑", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.634],
+        ["青绿", "蜷缩", "沉闷", "清晰", "凹陷", "硬滑", 0.608],
+        ["浅白", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.556],
+        ["青绿", "稍蜷", "浊响", "清晰", "稍凹", "软粘", 0.403],
+        ["乌黑", "稍蜷", "浊响", "稍糊", "稍凹", "软粘", 0.481],
+        ["乌黑", "稍蜷", "浊响", "清晰", "稍凹", "硬滑", 0.437],
+        ["乌黑", "稍蜷", "沉闷", "稍糊", "稍凹", "硬滑", 0.666],
+        ["青绿", "硬挺", "清脆", "清晰", "平坦", "软粘", 0.243],
+        ["浅白", "硬挺", "清脆", "模糊", "平坦", "硬滑", 0.245],
+        ["浅白", "蜷缩", "浊响", "模糊", "平坦", "软粘", 0.343],
+        ["青绿", "稍蜷", "浊响", "稍糊", "凹陷", "硬滑", 0.639],
+        ["浅白", "稍蜷", "沉闷", "稍糊", "凹陷", "硬滑", 0.657],
+        ["乌黑", "稍蜷", "浊响", "清晰", "稍凹", "软粘", 0.360],
+        ["浅白", "蜷缩", "浊响", "模糊", "平坦", "硬滑", 0.593]
+        ]
+data = np.array(data)
+label = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+# 属性集a
+a = [["青绿", "乌黑", "浅白"],
+     ["蜷缩", "稍蜷", "硬挺"],
+     ["浊响", "沉闷", "清脆"],
+     ["清晰", "稍糊", "模糊"],
+     ["凹陷", "稍凹", "平坦"],
+     ["硬滑", "软粘"]]
+a_label = ["色泽", "根蒂", "敲声", "纹理", "脐部", "触感", "密度"]
+
+# 密度连续值求最优
+# 密度划分点候选值
+density = []
+for i in range(len(data) - 1):
+    density.append((data[i][6] + data[i + 1][6]) / 2)
+# 把密度单独拿出来放到一个列表里
+density_data = data[:, 6]
+# for i in range(len(data)):
+#     density_data.append(data[i][6])
+# 求每个划分点的信息增益
+density_result = []
+for x in density:
+    density_result.append(ContinuationGain(density_data, label, x))
+# 求最优增益点
+density_value, density_point = MaxIndex(density_result)
+density_point = density[density_point]
