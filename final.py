656cbafb446f69943c522952175651f25f97e086
foragony <1045952912@qq.com>
2021-04-01 14:33:42 +0800

    最后一次修改，格式修改了一下，代码美化了一下

--- final.py
+++ final.py
@@ -1,19 +1,12 @@
 """
 file: tree.py
 author: 1851995刘佳航
-基于信息熵进行划分选择的决策树算法,无剪枝
+基于信息熵进行划分选择的ID3决策树算法
 """
 
 import numpy as np
 import copy
 from graphviz import Digraph
-
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 
 """
 决策树数据结构构造
@@ -22,6 +15,11 @@ class TreeNode(object):
 先找到最优的划分属性，然后这样构造字典
 treenode = {最佳属性：{里面是属性的各个取值}}
 本来想利用类来构造决策树，但是怎么构造没有思路，就去网上找了字典的构造方法
+"""
+
+"""
+以下为graphviz作图使用函数
+目的是生成的字典决策树进行树状展示
 """
 
 
@@ -66,8 +64,17 @@ def _sub_plot(g, tree, inc):
             g.edge(inc, root, str(i))
 
 
+"""
+以下为生成决策树所需函数
+"""
+
 
 def MaxIndex(_data):
+    """
+    获取列表中最大值以及最大值所在位置
+    :param _data: list
+    :return: float, int
+    """
     _max = _data[0]
     _index = 0
     for i in range(len(_data)):
@@ -80,7 +87,7 @@ def MaxIndex(_data):
 def EntD(_label):
     """
     计算信息熵
-    :param _label: list
+    :param _label: list 所要计算样本集中对应的标签
     :return: double
     """
     length = len(_label)
@@ -96,6 +103,13 @@ def EntD(_label):
 
 
 def Gain(_data, _label, _data_label):
+    """
+    计算离散变量信息增益
+    :param _data: 某一离散变量对应样本集
+    :param _label: 某一离散变量对应样本集的标签
+    :param _data_label: 某一离散变量对应的不同取值
+    :return:
+    """
     _length = len(_label)
     _result = 0.0
     _each_data = []
@@ -114,9 +128,10 @@ def Gain(_data, _label, _data_label):
 
 def ContinuationGain(_data, _label, _value):
     """
-    :param _value: double
-    :param _data:  ndarray
-    :param _label:  list
+    计算连续变量的信息增益
+    :param _value: double 连续变量的取值
+    :param _data:  ndarray 某一连续变量对应样本集
+    :param _label:  list 某一连续变量对应样本集的标签
     :return:
     """
     _length = len(_label)
@@ -139,32 +154,35 @@ def ContinuationGain(_data, _label, _value):
 
 
 def CGain(density, _label):
+    """
+    计算连续变量的增益，目的是选出最优划分
+    :param density: 连续变量样本集
+    :param _label: 样本集对应标签
+    :return:
+    """
     # 密度连续值求最优
     # 密度划分点候选值
     density_data = []
-    # density = data[:, 6]
     density = np.array(density, dtype="float32")
+    # 排序进行取值选择
     density1 = np.sort(density)
     for i in range(len(_label) - 1):
         density_data.append((density1[i] + density1[i + 1]) / 2)
-    print(density_data)
     # 求每个划分点的信息增益
     density_result = []
     for x in density_data:
         density_result.append(ContinuationGain(density, _label, x))
-    print(density_result)
     # 求最优增益点
     density_value, density_point = MaxIndex(density_result)
     density_point = density_data[density_point]
-    print(density_value)
     return density_value, density_point
 
 
 def CheckSame(_data, _attribute_label):
     """
-
+    检查样本集中属性是否相等
     :param _data: ndarry
-    :param _attribute_label:
+    :param _attribute_label: 每个属性
     :return:
     """
     _length = len(_attribute_label)
@@ -179,9 +197,9 @@ def CheckSame(_data, _attribute_label):
 def BestAttribute(_data, _label, _attribute, _attribute_label):
     """
     选出最佳的属性
-    :param _data:ndarray
+    :param _data:ndarray 样本
     :param _label:
-    :param _attribute:
+    :param _attribute:属性
     :param _attribute_label:
     :return:
     """
@@ -209,52 +227,45 @@ def TreeGenerate(_data, _label, _attribute, _attribute_label):
             return "好瓜"
         else:
             return "坏瓜"
+    # _index最优属性的序号，_best_attribute最优属性，_attribute返回的新属性的取值集，目的是如果是连续属性需要增加新取值
     _best_attribute, _index, _attribute = BestAttribute(_data, _label, _attribute, _attribute_label)
     tree_node = {_best_attribute: {}}  # 创建一个树（字典），bestFeatLabel为根结点
-    # _index = 1  # 最优属性的序号，待修改
     for x in _attribute[_index]:
         _son = []
         _son_label = []
         if _best_attribute == "密度":
-            midu = float(x)
-            x = str(x)
-            if midu < 0:
+            # midu = float(x)
+            # x = str(x)
+            if x < 0:
                 for i in range(len(_label)):
-                    if float(_data[i, _index]) <= -midu:
+                    if float(_data[i, _index]) <= -x:
                         _son.append(_data[i, :])
                         _son_label.append(_label[i])
             else:
                 for i in range(len(_label)):
-                    if float(_data[i, _index]) >= midu:
+                    if float(_data[i, _index]) >= x:
                         _son.append(_data[i, :])
                         _son_label.append(_label[i])
+            x = str(x)
         else:
             for i in range(len(_label)):
                 if x == _data[i, _index]:
                     _son.append(_data[i, :])
                     _son_label.append(_label[i])
-                # _data = np.delete(_data, i, axis=0)
-        # _son = np.array(_son)
-        # print("son:", _son)
         if not _son:
             if _son_label.count(1) > _son_label.count(0):
                 tree_node[_best_attribute][x] = "好瓜"
             else:
                 tree_node[_best_attribute][x] = "坏瓜"
             return tree_node
-            # tree_node[_best_attribute][x] = 1  # 最多的类，未写
         else:
-            # 写去掉a*
+            # 去掉a*
             _son = np.array(_son)
-            # print("son:", _son)
             _son = np.delete(_son, _index, axis=1)
-            # _data = np.delete(_data, i, axis=0)
             _attribute1 = copy.deepcopy(_attribute)
             del _attribute1[_index]
-            # _attribute = np.delete(_attribute, _index, axis=0)
             _attribute_label1 = copy.deepcopy(_attribute_label)
             del _attribute_label1[_index]
-            # _attribute_label = np.delete(_attribute_label, _index)
             tree_node[_best_attribute][x] = TreeGenerate(_son, _son_label, _attribute1, _attribute_label1)
     return tree_node
 
@@ -278,7 +289,7 @@ if __name__ == "__main__":
             ["浅白", "蜷缩", "浊响", "模糊", "平坦", "硬滑", 0.593]
             ]
     data = np.array(data)
-    print(data)
+    # 标签，1为好瓜，0为坏瓜
     label = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
     # 属性集a
     attribute = [["青绿", "乌黑", "浅白"],
@@ -287,32 +298,7 @@ if __name__ == "__main__":
                  ["清晰", "稍糊", "模糊"],
                  ["凹陷", "稍凹", "平坦"],
                  ["硬滑", "软粘"]]
-    # attribute = np.array(attribute, dtype="str")
-    print(attribute)
     attribute_label = ["色泽", "根蒂", "敲声", "纹理", "脐部", "触感", "密度"]
-    # attribute_label = np.array(attribute_label)
-    tree = TreeGenerate(data, label, attribute, attribute_label)
-    # print(TreeGenerate(data, label, attribute, attribute_label))
-    plot_model(tree, "test.gv")
-    # print(json.dumps(tree, indent=1))
-
-# def CGain(density, _label):
-#     # 密度连续值求最优
-#     # 密度划分点候选值
-#     density_data = []
-#     # density = data[:, 6]
-#     density = np.array(density, dtype="float32")
-#     density1 = np.sort(density)
-#     for i in range(len(_label) - 1):
-#         density_data.append((density1[i] + density1[i + 1]) / 2)
-#     print(density_data)
-#     # 求每个划分点的信息增益
-#     density_result = []
-#     for x in density_data:
-#         density_result.append(ContinuationGain(density, _label, x))
-#     print(density_result)
-#     # 求最优增益点
-#     density_value, density_point = MaxIndex(density_result)
-#     density_point = density[density_point]
-#     print(density_value)
-#     return density_value, density_point
+    mytree = TreeGenerate(data, label, attribute, attribute_label)
+    print(mytree)
+    plot_model(mytree, "mytree.gv")
